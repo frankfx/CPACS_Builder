a1c3a69406af7c2e011f3bfd502f5e18ef45596d
Rinifx@gmail.com <Rinifx@gmail.com>
2016-12-02 21:05:17 +0100

    default message

--- ComperatorTest/src/MahnbelegartComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class MahnbelegartComparator implements Comparator<ForderungVerbindlichkeitModel> {
-
-	@Override
-	public int compare(ForderungVerbindlichkeitModel o1, ForderungVerbindlichkeitModel o2) {
-		return o1.getMahnbelegart().toString().compareTo(o2.getMahnbelegart().toString());
-	}
-}
